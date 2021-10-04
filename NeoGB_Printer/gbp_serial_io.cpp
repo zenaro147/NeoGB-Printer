28bcac716c525073cd6c9e06c39bc6b7c9d0f8f7
Rafael Zenaro <rzoliveira93@gmail.com>
2021-10-04 14:46:24 -0300

    Update gbp_serial_io.cpp
    Using the most recent GBP_SERIAL_IO.CPP from @mofosyne

--- NeoGB_Printer/gbp_serial_io.cpp
+++ NeoGB_Printer/gbp_serial_io.cpp
@@ -192,11 +192,14 @@ static uint8_t gpb_sio_getByte(const int bytePos)
 
 bool gbp_serial_io_timeout_handler(uint32_t elapsed_ms)
 {
+  
+#if 0 // This redundancy causes an infinite loop in (Tsuri Seensei 2) GH-57
   if (gpb_pktIO.breakPacketReceived)
   {
-    //gpb_serial_io_reset();////////////////////////Raphael BOICHOT////////////fix 18/08/2021//////////////
-    //return false;         ////////////////////////Raphael BOICHOT////////////fix 18/08/2021//////////////
+    gpb_serial_io_reset();
+    return true;
   }
+#endif
 
   if (gpb_pktIO.timeout_ms > 0)
   {
@@ -562,7 +565,7 @@ bool gpb_serial_io_OnChange_ISR(const bool GBP_SCLK, const bool GBP_SOUT)
           gpb_pktIO.untransPacketCountdown = 0;
           gpb_pktIO.busyPacketCountdown = 0;
           gpb_status_bit_update_print_buffer_full(gpb_pktIO.statusBuffer, false);
-          gpb_status_bit_update_printer_busy(gpb_pktIO.statusBuffer, false);////////////////////////Raphael BOICHOT////////////fix 18/08/2021//////////////
+          gpb_status_bit_update_printer_busy(gpb_pktIO.statusBuffer, false);
           break;
         case GBP_COMMAND_PRINT:
           gpb_pktIO.busyPacketCountdown = GBP_BUSY_PACKET_COUNT;
@@ -579,7 +582,7 @@ bool gpb_serial_io_OnChange_ISR(const bool GBP_SCLK, const bool GBP_SOUT)
           gpb_status_bit_update_print_buffer_full(gpb_pktIO.statusBuffer, true);
           gpb_status_bit_update_printer_busy(gpb_pktIO.statusBuffer, true);
           gpb_status_bit_update_checksum_error(gpb_pktIO.statusBuffer, false);
-          case GBP_COMMAND_INQUIRY:
+        case GBP_COMMAND_INQUIRY:
           /*if (gpb_pktIO.untransPacketCountdown > 0)
           {
             gpb_pktIO.untransPacketCountdown--;
@@ -601,7 +604,7 @@ bool gpb_serial_io_OnChange_ISR(const bool GBP_SCLK, const bool GBP_SOUT)
               gpb_status_bit_update_printer_busy(gpb_pktIO.statusBuffer, false);
             }
           }*/
-      if (gpb_pktIO.busyPacketCountdown > 0)
+          if (gpb_pktIO.busyPacketCountdown > 0)
           {
             gpb_status_bit_update_unprocessed_data(gpb_pktIO.statusBuffer, false);
             gpb_status_bit_update_printer_busy(gpb_pktIO.statusBuffer, true);
@@ -651,7 +654,7 @@ bool gpb_serial_io_OnChange_ISR(const bool GBP_SCLK, const bool GBP_SOUT)
         case GBP_COMMAND_BREAK:
         break;
         case GBP_COMMAND_INQUIRY:
-            gpb_status_bit_update_unprocessed_data(gpb_pktIO.statusBuffer, false);
+          gpb_status_bit_update_unprocessed_data(gpb_pktIO.statusBuffer, false);
           if ((gpb_pktIO.untransPacketCountdown == 0) && (gpb_pktIO.busyPacketCountdown == 0))
           {
             gpb_status_bit_update_print_buffer_full(gpb_pktIO.statusBuffer, false);
