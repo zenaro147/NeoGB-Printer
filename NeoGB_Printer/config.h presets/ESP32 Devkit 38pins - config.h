21da5a206ee12720af017c80746f8f457fd32173
Rafael Zenaro <rzoliveira93@gmail.com>
2022-05-13 14:16:30 -0300

    Presets configs

--- NeoGB_Printer/config.h presets/ESP32 Devkit 38pins - config.h
+++ NeoGB_Printer/config.h presets/ESP32 Devkit 38pins - config.h
@@ -1,7 +1,7 @@
 /*********************************
  * GAMEBOY LINK CABLE DEFINITIONS
  *********************************/
-// Note: Serial Clock Pin must be attached to an interrupt pin of the arduino (Pins 1 and 4 are not used)
+// Note: Serial Clock Pin must be attached to an interrupt pin of the ESP (Pins 1 and 4 are not used. Pin 6 must be connect to the GND)
 //  ___________
 // |  6  4  2  |
 //  \_5__3__1_/   (at cable)
@@ -24,22 +24,22 @@
  * PUSH BUTTON AND IMAGE DEFINITIONS 
  ************************************/
 #define BTN_PUSH 34             // Define a PushButton to use to Force a new file/Generate the images.
-//#define BTN_INVERT            //Define to use the LOW state instead HIGH state. Useful for the TTGO board.
-#define BMP_UPSCALE_FACTOR 1    //Set the Upscale factor for the BMP output (Set 0 to disable)
-#define PNG_UPSCALE_FACTOR 1    //Set the Upscale factor for the PNG output (Set 0 to disable)
+//#define BTN_INVERT           //Define to use the LOW state instead HIGH state. Useful for the TTGO board.
+#define BMP_UPSCALE_FACTOR 0    //Set the Upscale factor for the BMP output (Set 0 to disable)
+#define PNG_UPSCALE_FACTOR 4    //Set the Upscale factor for the PNG output (Set 0 to disable)
 
 /***************************
  * OLED DISPLAY DEFINITIONS
  ***************************/
-//#define USE_OLED        //Enable OLED
+#define USE_OLED        //Enable OLED
 
-//Uncomment this if you are using SSD1306 display driver (I2C)
-//#define USE_SSD1306
-//#define OLED_SDA 21
-//#define OLED_SCL 22
-//#define OLED_COMPINS 1 //If you have some issues with the image, try to change this with values between 1 and 4
+//Uncomment this if you are using SSD1306 display driver
+#define USE_SSD1306
+#define OLED_SDA 21
+#define OLED_SCL 22
+#define OLED_COMPINS 1      // If you have some issues with the image, try to change this with values between 1 and 4
 
-//Uncomment this if you are using SSD1331 display driver (SPI)
+//Uncomment this if you are using SSD1331 display driver
 //#define USE_SSD1331
 //#define OLED_SCLK 14
 //#define OLED_MOSI 13
@@ -49,9 +49,9 @@
 //#define OLED_RST   4
 
 //Set your display dimensions
-//#define SCREEN_WIDTH 128
-//#define SCREEN_HEIGHT 32
-//#define OLED_ROTATE   //Rotate the display by 180°
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_ROTATE       //Rotate the display by 180°
 
 /*************************
  * LED STATUS DEFINITIONS
@@ -60,20 +60,20 @@
 //#define LED_STATUS_PIN    5       // LED blink on packet reception (2 is internal LED)
 
 // RGB LED Definitions
-//#define LED_STATUS_BLUE  17
-//#define LED_STATUS_RED   16
-//#define LED_STATUS_GREEN 4
+#define LED_STATUS_BLUE  17
+#define LED_STATUS_RED   16
+#define LED_STATUS_GREEN 4
 //#define COMMON_ANODE
-//#define COMMON_CATHODE
+#define COMMON_CATHODE
 
 /*************************
  * WEB SERVER DEFINITIONS
  *************************/
-//#define ENABLE_WEBSERVER
-#define WIFI_CONNECT_TIMEOUT 10000              //Connection Timeout in ms
+#define ENABLE_WEBSERVER         
+#define WIFI_CONNECT_TIMEOUT 10000  //Connection Timeout in ms
 
 /*************************
  * RTC DEFINITIONS
  *************************/
- #define RTC_TIMEZONE -3 //Define your timezone
- #define RTC_TIMEDIFF 0 //Define the time difference between the NTP server and the real time
+ #define RTC_TIMEZONE -3      //Define your timezone
+ #define RTC_TIMEDIFF 1080    //Define the time difference between the NTP server and the real time
