5c903edde98a3339b46b311aaf4523d39e17c6ea
Rafael Zenaro <rzoliveira93@gmail.com>
2022-12-19 13:00:33 -0300

    Update NeoGB_Printer.ino

--- NeoGB_Printer/NeoGB_Printer.ino
+++ NeoGB_Printer/NeoGB_Printer.ino
@@ -193,6 +193,23 @@ void setup(void){
       full();
       delay(5000);
     }
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    Serial.println("Checking for dumps to convert...");
+    freeFileIndex = get_next_ID();
+    dumpCount = get_dumps();
+    if ((freeFileIndex-1) > 0 && dumpCount > 0){
+      Serial.println("Converting dumps to image files");
+      isConverting = true;
+      #ifdef USE_OLED
+        oledStateChange(5); //BIN to BMP
+      #endif
+      ConvertFilesBMP();
+      Serial.println("Done!"); 
+    }else{
+      Serial.println("No dumps found!");
+    }
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
     if (bootAsPrinter){
       //initWifi(); //Initiate WiFi and NTP
@@ -241,24 +258,6 @@ void setup(void){
         Serial.println("-----------------------");
         Serial.println("Booting in server mode");
         Serial.println("-----------------------");
-
-///////////////////////////////////////////////////////////////////////////////////////////////////        
-  Serial.println("Checking for dumps to convert...");
-  //if (!isConverting && !isPrinting && (freeFileIndex-1) > 0 && dumpCount > 0){
-      freeFileIndex = get_next_ID();
-      dumpCount = get_dumps();
-  if ((freeFileIndex-1) > 0 && dumpCount > 0){
-    Serial.println("Converting dumps to image files");
-    isConverting = true;
-    #ifdef USE_OLED
-      oledStateChange(5); //BIN to BMP
-    #endif
-    ConvertFilesBMP();
-    Serial.println("Done!"); 
-  }else{
-    Serial.println("No dumps found!");
-  }
-/////////////////////////////////////////////////////////////////////////////////////////////////////        
 
         mdns_setup();
         webserver_setup();
